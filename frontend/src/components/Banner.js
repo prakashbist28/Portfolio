9533155e21c3cdc1cc10ee3a551089d8af6292fc
Prakash Bist <prakashcena540@gmail.com>
2023-08-09 19:20:15 +0530

    Update Banner.js

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -67,7 +67,7 @@ const Banner = () => {
             className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0">
               <button className="btn btn-lg" onClick={handleClick}>Contact Me</button>
 
-              <a href="https://drive.google.com/file/d/12mfeIANyQltuyoLERpYSEHF5FlZ3oPR8/view?usp=sharing" className="text-gradient btn-link" target='_blank'>
+              <a href="https://drive.google.com/file/d/1ydWWM7xqCsw7rJpd6ccbmm8bUjRtqKSY/view?usp=sharing" className="text-gradient btn-link" target='_blank'>
                 {" "}
                 My Resume{" "}
               </a>
